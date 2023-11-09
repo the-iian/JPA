03901dd7de0330b24c47b7f922702d9b9ed76e69
iian <yiaahn@gmail.com>
2023-11-09 13:21:51 +0900

    기본키 매핑 - Table (@TableGenerator)

--- src/main/java/com/jpa/ex1hello/hellojpa/JpaMain.java
+++ src/main/java/com/jpa/ex1hello/hellojpa/JpaMain.java
@@ -11,10 +11,9 @@ public class JpaMain {
     public static void main(String[] args){
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-
         EntityManager em = emf.createEntityManager();
-
         EntityTransaction tx = em.getTransaction();
+
         tx.begin();
 
         try{
